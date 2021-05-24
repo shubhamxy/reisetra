f840c52f1f94cccb9597c2f047fff672af77f3f8
Shubham Jain <25664213+shubhamxy@users.noreply.github.com>
2021-05-25 04:56:29 +0530

    ci(global): eNV Vars for nextjs

--- mint/src/libs/rock/config/index.tsx
+++ mint/src/libs/rock/config/index.tsx
@@ -22,19 +22,19 @@ export const isBrowser = typeof window !== undefined;
 
 export const config: Config = {
   // Common
-  isProduction: process.env.NODE_ENV === "production",
-  name: process.env.APP_NAME || "Reisetra",
-  description: process.env.APP_DESCRIPTION || "Unique products designed by independent artists.",
-  contactEmail: "contact@reisetra.com",
-  appEnv: (process.env.APP_ENV || "production") as Environment,
+  isProduction: process.env.NEXT_PUBLIC_NODE_ENV === "production",
+  name: process.env.NEXT_PUBLIC_APP_NAME || "Reisetra",
+  description: process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Unique products designed by independent artists.",
+  contactEmail: process.env.NEXT_PUBLIC_CONTACT_EMAIL || "contact@reisetra.com",
+  appEnv: (process.env.NEXT_PUBLIC_APP_ENV || "production") as Environment,
   port: +process.env.PORT || 3000,
-  clientUrl: process.env.CLIENT_URL || 'http://localhost:3000',
+  clientUrl: process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000',
 
   // API
-  apiUrl: process.env.API_URL || "http://localhost:8080/api/v1/",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1/",
 
   // GOOGLE Login
   googleOAuthOptions: {
-    clientID: process.env.GOOGLE_OAUTH_CLIENT_ID || '583554562558-6stke69s8nsc3lca6beajdu4ergs2msh.apps.googleusercontent.com',
+    clientID: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID || '583554562558-6stke69s8nsc3lca6beajdu4ergs2msh.apps.googleusercontent.com',
   },
 };
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-rsync -avz -e "ssh -i ~/.ssh/aws/Soda.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --exclude-from './scripts/exclude-list' --progress soda ubuntu@api.reisetra.com:~/server
+# rsync -avz -e "ssh -i ~/.ssh/aws/Soda.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --exclude-from './scripts/exclude-list' --progress soda ubuntu@api.reisetra.com:~/server
 
+# ssh -i "~/.ssh/aws/Soda.pem" ubuntu@api.reisetra.com
+
+_remote="ubuntu@api.reisetra.com"
+_user="ubuntu"
+
+echo "Local system name: $HOSTNAME"
+echo "Local date and time: $(date)"
+
+echo
+echo "*** Running commands on remote host named $_remote ***"
+echo
+ssh -i "~/.ssh/aws/Soda.pem" $_remote <<'EOL'
+  cd ~/server/soda
+  pm2 start ecosystem.config.js --env production
+  ls -la
+  nvm use default
+  yarn install
+  mv .env.production .env
+  yarn run start:prod
+  cd
+EOL
