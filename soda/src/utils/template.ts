acf6f1e62dea30e9a7da50278ced7d96afc2f210
Shubham Jain <25664213+shubhamxy@users.noreply.github.com>
2021-06-26 10:33:34 +0530

    fix(soda): fix email template

--- soda/src/utils/template.ts
+++ soda/src/utils/template.ts
@@ -2,7 +2,14 @@ import { readFileSync } from "fs";
 import handlebars from "handlebars";
 import { createParams, IParams } from "./aws";
 import { services, app } from "src/config";
-
+const images = {
+  hero: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/success.png',
+  authentication: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/authentication.png',
+  forgot: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/forgot.png',
+  login: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/login.png',
+  logo: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/logo.png',
+  welcome: 'https://raw-soda.s3.ap-south-1.amazonaws.com/static/welcome.png',
+};
 const common = readFileSync(process.env.NODE_PATH + "/views/mail/common.hbs", "utf8");
 const commonTemplate = handlebars.compile(common);
 const options = (
@@ -12,19 +19,21 @@ const options = (
     preheader: string;
     header: string;
     subheader: string;
+    logoImageSrc: string;
     heroImageSrc: string;
     heroLink: string;
     mainParagraph: string;
     mainCallToActionUrl: string;
     mainCallToActionText: string;
     footerText: string;
+    contactEmail: string;
   }
 ): Partial<IParams> => {
   const config = services();
   return {
     source: `"${config.mailer.senderName}" <${config.aws.sesEmailSender}>`,
     to: [email],
-    subject: `${config.mailer.senderName} | ${locals.messageSubject}`,
+    subject: `${locals.messageSubject || ''}`,
     html: commonTemplate(locals),
     text: `${locals.mainParagraph}`,
   };
@@ -39,12 +48,13 @@ export function passwordResetEmail(user: { email: string; token: string }) {
       preheader: ``,
       header: "Reset Password",
       subheader: `A password reset event has been triggered. The password reset window is limited to two hours.`,
-      heroImageSrc:
-        "https://media.giphy.com/media/Xev2JdopBxGj1LuGvt/source.gif",
+      logoImageSrc: images.logo,
+      heroImageSrc: images.authentication,
       heroLink: "https://reisetra.com",
-      mainParagraph: `To complete the password reset process, click on the button below or use this link ${passwordResetLink}`,
+      mainParagraph: `To complete the password reset process, click on the button below.`,
+      contactEmail: "contact@reisetra.com",
       mainCallToActionUrl: passwordResetLink,
-      mainCallToActionText: "Reset",
+      mainCallToActionText: "Reset Password",
       footerText: "This message was ment for " + user.email + ".",
     })
   );
@@ -61,15 +71,16 @@ export function emailVerificationEmail(user: {
     options(user.email, {
       messageSubject: "Confirm your email address",
       preheader: ``,
-      header: "Welcome to reisetra",
-      subheader: `Verify your e-mail to finish signing up for Reisetra`,
-      heroImageSrc:
-        "https://media.giphy.com/media/Xev2JdopBxGj1LuGvt/source.gif",
+      header: "Welcome",
+      subheader: `Verify your e-mail to finish signing up`,
+      logoImageSrc: images.logo,
+      heroImageSrc: images.welcome,
       heroLink: "https://reisetra.com",
-      mainParagraph: `Please confirm that ${user.email} is your e-mail address by clicking on the button below or use this link ${emailVerifyLink}`,
+      mainParagraph: `Please confirm that ${user.email} is your e-mail address by clicking on the button below.`,
       mainCallToActionUrl: emailVerifyLink,
-      mainCallToActionText: "Verify",
+      mainCallToActionText: "Verify Email",
       footerText: "This message was ment for " + user.email + ".",
+      contactEmail: "contact@reisetra.com",
     })
   );
 }
